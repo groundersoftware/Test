2d178b2c5710b7f5437edbf26da77ae146f0ea76
Stefan Novak <snovak@sygic.com>
2014-03-14 09:42:36 +0100

    Working Map!!!

--- MapTest/MapTest/MainActivity.cs
+++ MapTest/MapTest/MainActivity.cs
@@ -12,26 +12,37 @@ using Android.OS;
 namespace MapTest
 {
     [Activity(Label = "MapTest", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity 
+    public class MainActivity : Activity
     {
+        /// <summary>Map fragment</summary>
+        private MapFragment _mapFragment;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            var _myMapFragment = MapFragment.NewInstance();
-            FragmentTransaction tx = FragmentManager.BeginTransaction();
-            tx.Add(Resource.Id.map, _myMapFragment);
+            _mapFragment = MapFragment.NewInstance();
+            var tx = FragmentManager.BeginTransaction();
+            tx.Add(Resource.Id.map, _mapFragment);
             tx.Commit();
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+        }
 
-            //var mapFrag = (MapFragment) FragmentManager.FindFragmentById(Resource.Id.map);
-            //var map = mapFrag.Map;
-            //if (map != null)
-            //{
-            //    // The GoogleMap object is ready to go.
-            //}
+        protected override void OnResume()
+        {
+
+            var mapFrag = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
+            var map = mapFrag.Map;
+            if (map != null)
+            {
+                // The GoogleMap object is ready to go.
+                System.Console.WriteLine("Mam mapu!!!");
+            }
+
+
+            base.OnResume();
         }
     }
 }
